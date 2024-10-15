a6831e88faeb5901fa3534b78638564d786c810e
SsemakulaJonathan <uhurussemakula069@gmail.com>
2024-10-16 02:15:16 +0800

    Update configuration for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,36 +1,10 @@
-// const path = require('path')
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//   webpack: (config) => {
-//     config.resolve.alias['@'] = path.resolve(__dirname)
-//     return config
-//   },
-// }
-
-// module.exports = nextConfig
-
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   output: 'export',
-//   images: {
-//     unoptimized: true,
-//   },
-//   assetPrefix: process.env.NODE_ENV === 'production' ? '/' : '',
-//   basePath: process.env.NODE_ENV === 'production' ? '' : '',
-// }
-
-// module.exports = nextConfig
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
-  // Ensure your repository name is added to the base path if it's not using a custom domain yet
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/jonathan-portfolio' : '',
   basePath: process.env.NODE_ENV === 'production' ? '/jonathan-portfolio' : '',
 }
 
