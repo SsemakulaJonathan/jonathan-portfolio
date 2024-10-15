7c92da2d3a31e952d4fec24014ba37b354a34800
Ssemakula Jonathan <jonathan@Uhurus-MacBook-Pro.local>
2024-10-16 01:50:20 +0800

    Configure for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,12 +1,25 @@
-const path = require('path')
+// const path = require('path')
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   webpack: (config) => {
+//     config.resolve.alias['@'] = path.resolve(__dirname)
+//     return config
+//   },
+// }
+
+// module.exports = nextConfig
+
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  webpack: (config) => {
-    config.resolve.alias['@'] = path.resolve(__dirname)
-    return config
+  output: 'export',
+  images: {
+    unoptimized: true,
   },
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '' : '',
 }
 
 module.exports = nextConfig
