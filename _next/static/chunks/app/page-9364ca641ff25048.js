7c541c20f9d030a5d1c29ecce08353a794609a35
SsemakulaJonathan <SsemakulaJonathan@users.noreply.github.com>
2024-10-16 09:57:46 +0000

    deploy: f0de336e83e7670c3a3b01219c6a79f043accec5

---
+++ _next/static/chunks/app/page-9364ca641ff25048.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931,895,890],{8368:function(n,e,u){Promise.resolve().then(u.t.bind(u,2972,23))}},function(n){n.O(0,[972,971,117,744],function(){return n(n.s=8368)}),_N_E=n.O()}]);
