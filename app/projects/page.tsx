f25bc4a3d45a313d3a953a698f0da73697f2d7e7
SsemakulaJonathan <55960987+SsemakulaJonathan@users.noreply.github.com>
2024-10-16 19:32:42 +0800

    Update page.tsx

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -10,11 +10,11 @@ export default async function Projects() {
       <header className="flex items-center bg-white p-4 pb-2 justify-between">
         <h2 className="text-[#111418] text-lg font-bold leading-tight tracking-[-0.015em] flex-1 text-center pl-12">Projects</h2>
         <div className="flex w-12 items-center justify-end">
-          <button className="text-[#111418]">
+{/*           <button className="text-[#111418]">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 256 256">
               <path d="M227.31,73.37,182.63,28.68a16,16,0,0,0-22.63,0L36.69,152A15.86,15.86,0,0,0,32,163.31V208a16,16,0,0,0,16,16H92.69A15.86,15.86,0,0,0,104,219.31L227.31,96a16,16,0,0,0,0-22.63ZM92.69,208H48V163.31l88-88L180.69,120ZM192,108.68,147.31,64l24-24L216,84.68Z"></path>
             </svg>
-          </button>
+          </button> */}
         </div>
       </header>
 
